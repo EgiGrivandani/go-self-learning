1b4ad46faa42b65fbf1b0af6f9a2df3a3696863d
EgiGrivandani <egigrivandani0133@gmail.com>
2026-04-27 10:59:12 +0700

    deffer panic recover

---
+++ 1-dasar/31-defer-panic-recover/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func logging() {
+	fmt.Println("selesai memanggil function")
+}
+
+func runApp() {
+	defer logging()
+	fmt.Println("Run App")
+}
+
+func samplePanic() {
+	fmt.Println("\n=== Panic ===")
+	fmt.Println("Sebelum panic")
+	panic("Terjadi error yang tidak bisa ditangani!")
+}
+
+func sampleRecover() {
+	fmt.Println("\n=== Recover ===")
+
+	defer func() {
+		message := recover()
+		if message != nil {
+			fmt.Println("Panic ditangkap:", message)
+			fmt.Println("Program tetap berjalan!")
+		}
+	}()
+	fmt.Println("Sebelum panic")
+	panic("Error: sesuatu yang buruk terjadi")
+
+	fmt.Println("Pesan ini tidak akan di munculkan, karena sudah panic")
+}
+
+func main() {
+	runApp()
+
+	//samplePanic()
+
+	sampleRecover()
+	fmt.Println("Pesan ini tetap akan di munculkan, walaupun sudah panic")
+}
