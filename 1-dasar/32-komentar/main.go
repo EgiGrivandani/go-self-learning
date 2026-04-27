892fd8dade1084fc1f06c7c471e46ce8b24fcfd9
EgiGrivandani <egigrivandani0133@gmail.com>
2026-04-27 11:02:32 +0700

    komentar

---
+++ 1-dasar/32-komentar/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// Ini adalah komentar satu baris
+	// Go akan mengabaikan semua teks setelah //
+
+	/*
+		Ini adalah komentar banyak baris.
+		Bisa dipakai untuk menulis penjelasan panjang.
+		Semua teks di antara tanda ini diabaikan oleh Go.
+	*/
+
+}
