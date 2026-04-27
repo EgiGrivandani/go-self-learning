035816a668d82632c9d2e9e378efaf26e176f4ff
EgiGrivandani <egigrivandani0133@gmail.com>
2026-04-27 14:45:50 +0700

    latihan

--- 1-dasar/35-interface/main.go
+++ 1-dasar/35-interface/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Hewan interface {
 	suara() string
 	jenis() string
@@ -15,12 +17,31 @@ func (k Kucing) jenis() string {
 	return "Mamalia"
 }
 
-func cetakSuara(h Hewan) {
-	println("Suara:", h.suara())
-	println("Jenis:", h.jenis())
+type Anjing struct{}
+
+func (k Anjing) suara() string {
+	return "Guk Guk"
+}
+
+func (k Anjing) jenis() string {
+	return "Mamalia"
+}
+
+type Burung struct{}
+
+func (k Burung) suara() string {
+	return "Cuit Cuit"
+}
+
+func (k Burung) jenis() string {
+	return "Aves"
 }
 
 func main() {
-	k := Kucing{}
-	cetakSuara(k)
+	//LATIHAN : Buat interface Hewan dengan method Suara() string dan Jenis() string. Implementasikan untuk struct Kucing, Anjing, dan Burung. Buat slice []Hewan dan cetak suara semua hewan.
+	daftarHewan := []Hewan{Kucing{}, Anjing{}, Burung{}}
+
+	for _, h := range daftarHewan {
+		fmt.Printf("Suara %s | Jenis %s \n", h.suara(), h.jenis())
+	}
 }
